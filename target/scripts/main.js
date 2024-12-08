ddbbf03564e084a844f171afaf37cf508f1ffab3
Pico-Moon <P.m381401217232@gmail.com>
2024-12-09 06:40:38 +0800

    Add files via upload

---
+++ target/scripts/main.js
@@ -0,0 +1,23 @@
+var myHeading = document.querySelector("h5");
+
+
+/*function setUserName() {
+  let myName = prompt("Please enter your name.");
+  if(!myName || myName === null) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Hello, ' + myName;
+  }
+}
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem("name");
+    myHeading.innerHTML = "Hello, " + storedName;
+}*/
+
+
+
+
